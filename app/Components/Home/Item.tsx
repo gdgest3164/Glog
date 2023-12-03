28371f4e3536a93754f6d02b7c3820e183da51b1
gdgest3164 <rlaehdgs12@naver.com>
2023-12-03 16:03:42 +0900

    fix. 홈화면 애니메이션 적용

--- app/Components/Home/Item.tsx
+++ app/Components/Home/Item.tsx
@@ -4,12 +4,13 @@ import Image from "../Images/Image";
 export default function Item(post: any) {
   return (
     post && (
-      <article key={post.post.id}>
+      <article key={post.post.id} className="transition-all rounded-lg  group">
         <Link to={`/${post.post.id}`}>
-          <div className="ab tn" style={{ borderRadius: "10px", overflow: "hidden" }}>
+          <div className="ab tn rounded-lg overflow-hidden relative transition duration-300 transform group-hover:-translate-y-6">
             <Image src={post.post.imageUrl} />
+            {/* <div className="absolute inset-0 z-10 w-full h-full bg-gradient-to-t from-gray-950"></div> */}
           </div>
-          <p className="mt-3 line-clamp-3 leading-6 ">{post.post.title}</p>
+          <p className="mt-3 line-clamp-3 leading-6 transition duration-700 transform group-hover:-translate-y-4">{post.post.title}</p>
 
           <div className="flex justify-between w-full">
             <div className="mt-3 group relative flex gap-2 items-center">
@@ -17,11 +18,14 @@ export default function Item(post: any) {
                 <span className="absolute inset-0" />
                 {post.post.title}
               </h3> */}
-              <Link to={`/${post.post.category.id}`} className="text-sm relative z-10 rounded-full bg-gray-50 px-3 py-1 font-medium text-gray-600 hover:bg-gray-100">
+              <Link
+                to={`/${post.post.category.id}`}
+                className="text-sm relative z-10 rounded-full bg-gray-50 px-3 py-1 font-medium text-gray-600 hover:bg-gray-100 transition duration-1000 transform group-hover:-translate-y-2"
+              >
                 {post.post.category.title}
               </Link>
             </div>
-            <div className="flex items-center gap-x-4 text-xs mt-3">
+            <div className="flex items-center gap-x-4 text-xs mt-3 transition duration-1000 transform group-hover:-translate-y-2">
               <time>{new Date(post.post.created_at).toLocaleDateString("ko-KR", { year: "numeric", month: "2-digit", day: "2-digit" })}</time>
             </div>
           </div>
